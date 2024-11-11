946a027d84061e75a8078f2700a757cbcace6a6e
walyson moises <walysonmoises@gmail.com>
2024-11-11 14:41:50 -0300

    feat(auth): implement password recovery email sending with NodeMailer

--- backend/src/auth/infrastructure/utils/nodemailer.service.ts
+++ backend/src/auth/infrastructure/utils/nodemailer.service.ts
@@ -3,11 +3,31 @@ import {
   IEmailService,
   ImailData,
 } from 'src/auth/core/interfaces/recover-password/email-service.interface';
+import * as nodemailer from 'nodemailer';
 
 @Injectable()
 export class NodeMailerService implements IEmailService {
-  send(data: ImailData): Promise<void> {
-    console.log(data);
-    return;
+  private transporter;
+
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+  }
+
+  async send(data: ImailData): Promise<void> {
+    const mailOptions = {
+      from: 'Recipes',
+      to: data.to,
+      subject: data.subject,
+      html: data.body,
+    };
+    return await this.transporter.sendMail(mailOptions);
   }
 }

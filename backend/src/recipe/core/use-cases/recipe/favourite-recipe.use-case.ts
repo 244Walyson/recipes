fea72793ec45a3c7bc20f2f91f121653cd91ddf9
walyson moises <walysonmoises@gmail.com>
2024-11-30 16:56:17 -0300

    refactor(backend): extract duplicated logic into a shared validator for favourite/unfavourite use cases
    - Moved common validation logic into a single validator function.
    - Replaced duplicated validation logic in both favourite-recipe and unfavourite-recipe use cases.

--- backend/src/recipe/core/use-cases/recipe/favourite-recipe.use-case.ts
+++ backend/src/recipe/core/use-cases/recipe/favourite-recipe.use-case.ts
@@ -1,38 +1,20 @@
 import { RecipeDomainException } from '../../exceptions/domain.exception';
-import { RecipeInvalidFieldValueException } from '../../exceptions/invalid-field-value.exception';
 import { IRecipeRepository } from '../../interfaces/repositories/recipe.repository';
-import { FindRecipeByIdUseCase } from './find-recipe-by-id.use-case';
+import { RecipeValidator } from '../../validators/recipe.validator';
 
 export class FavouriteRecipeUseCase {
   constructor(
     private readonly recipeRepository: IRecipeRepository,
-    private readonly findRecipeByIdUseCase: FindRecipeByIdUseCase,
+    private readonly recipeValidator: RecipeValidator,
   ) {}
 
   async execute(dto: { recipeId: string; userId: string }): Promise<void> {
-    await this.validate(dto);
-    await this.findRecipeByIdUseCase.execute(dto.recipeId);
+    await this.recipeValidator.validate(dto);
     try {
       await this.recipeRepository.favouriteRecipe(dto.recipeId, dto.userId);
     } catch (error) {
       console.log(error);
       throw new RecipeDomainException('Error adding favorite recipe');
-    }
-  }
-
-  private async validate(dto: {
-    recipeId: string;
-    userId: string;
-  }): Promise<void> {
-    const errorMessages: Record<string, string>[] = [];
-    if (!dto.recipeId) {
-      errorMessages.push({ recipeId: 'recipeId não pode ser vazio' });
-    }
-    if (!dto.userId) {
-      errorMessages.push({ userId: 'userId não pode ser vazio' });
-    }
-    if (errorMessages.length) {
-      throw new RecipeInvalidFieldValueException(errorMessages);
     }
   }
 }

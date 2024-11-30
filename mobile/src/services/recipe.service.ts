074ea644908913d26e9f384370d3c050bd23f5b8
walyson moises <walysonmoises@gmail.com>
2024-11-30 17:31:12 -0300

    refactor(mobile): apply DRY principle to recipe service functions - extracted common logic into a helper function - simplified recipe API calls - improved readability and maintainability

--- mobile/src/services/recipe.service.ts
+++ mobile/src/services/recipe.service.ts
@@ -1,16 +1,9 @@
+import { IFindAllFilters } from "../interfaces/recipe/find-all-filters.interface";
+import { makeRequest } from "./helpers";
 import { API_URL } from "../utils/system";
 import { IReciperequest } from "../interfaces/recipe/recipe-request.interface";
-import { IFindAllFilters } from "../interfaces/recipe/find-all-filters.interface";
-import axiosIntance from "./interceptors";
-
 export const getRecipeById = async (id: string) => {
-  try {
-    const response = await axiosIntance.get(`${API_URL}/recipes/${id}`);
-    return response.data;
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
+  return await makeRequest("get", `${API_URL}/recipes/${id}`);
 };
 
 export const getRecipes = async (
@@ -18,142 +11,64 @@ export const getRecipes = async (
   page?: number,
   limit?: number
 ) => {
-  try {
-    const queryParams = new URLSearchParams();
+  const queryParams = new URLSearchParams();
+  if (page) queryParams.append("page", page.toString());
+  if (limit) queryParams.append("limit", limit.toString());
+  if (params.name) queryParams.append("name", params.name);
+  if (params.ingredients)
+    queryParams.append("ingredients", params.ingredients.join(","));
+  if (params.mealTypes)
+    queryParams.append("mealTypes", params.mealTypes.join(","));
+  if (params.price) queryParams.append("price", params.price.join(","));
+  if (params.servingCount)
+    queryParams.append("servingCount", params.servingCount);
+  if (params.allergens)
+    queryParams.append("allergens", params.allergens.join(","));
+  if (params.preparationTime)
+    queryParams.append("preparationTime", params.preparationTime.toString());
+  if (params.orderBy) queryParams.append("orderBy", params.orderBy);
+  console.log("queryParams", queryParams.toString());
 
-    if (page) queryParams.append("page", page.toString());
-    if (limit) queryParams.append("limit", limit.toString());
-
-    if (params.name) queryParams.append("name", params.name);
-    if (params.ingredients)
-      queryParams.append("ingredients", params.ingredients.join(","));
-    if (params.mealTypes)
-      queryParams.append("mealTypes", params.mealTypes.join(","));
-    if (params.price) queryParams.append("price", params.price.join(","));
-    if (params.servingCount)
-      queryParams.append("servingCount", params.servingCount);
-    if (params.allergens)
-      queryParams.append("allergens", params.allergens.join(","));
-    if (params.preparationTime)
-      queryParams.append("preparationTime", params.preparationTime.toString());
-    if (params.orderBy) queryParams.append("orderBy", params.orderBy);
-
-    console.log("queryParams", queryParams.toString());
-
-    const response = await axiosIntance.get(
-      `${API_URL}/recipes?${queryParams.toString()}`
-    );
-    return response.data;
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
-};
-
-export const getRecipesByUserId = async (userId: string) => {
-  try {
-    const response = await axiosIntance.get(
-      `${API_URL}/recipes/user/${userId}`
-    );
-    return response.data;
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
-};
-
-export const getTrendingecipes = async () => {
-  try {
-    const response = await axiosIntance.get(`${API_URL}/recipes`);
-    return response.data;
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
+  return await makeRequest(
+    "get",
+    `${API_URL}/recipes?${queryParams.toString()}`
+  );
 };
 
 export const createRecipe = async (recipe: IReciperequest) => {
-  try {
-    const response = await axiosIntance.post(`${API_URL}/recipes`, recipe);
-    return response.data;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
+  return await makeRequest("post", `${API_URL}/recipes`, recipe);
+};
+
+export const getRecipesByUserId = async (userId: string) => {
+  return await makeRequest("get", `${API_URL}/recipes/user/${userId}`);
+};
+
+export const getRecipesFavouritedByUserId = async (userId: string) => {
+  return await makeRequest("get", `${API_URL}/recipes/favourites/${userId}`);
 };
 
 export const updateRecipe = async (
   recipeId: string,
   recipe: IReciperequest
 ) => {
-  try {
-    const response = await axiosIntance.put(
-      `${API_URL}/recipes/${recipeId}`,
-      recipe
-    );
-    return response.data;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
-};
-
-export const viewRecipe = async (recipeId: string) => {
-  try {
-    const response = await axiosIntance.post(
-      `${API_URL}/recipes/views/${recipeId}`
-    );
-    return response.data;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
+  return await makeRequest("put", `${API_URL}/recipes/${recipeId}`, recipe);
 };
 
 export const favouriteRecipe = async (recipeId: string) => {
-  try {
-    const response = await axiosIntance.post(
-      `${API_URL}/recipes/favourites/${recipeId}`
-    );
-    return response.data;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
+  return await makeRequest("post", `${API_URL}/recipes/favourites/${recipeId}`);
 };
 
 export const unfavouriteRecipe = async (recipeId: string) => {
-  try {
-    const response = await axiosIntance.delete(
-      `${API_URL}/recipes/favourites/${recipeId}`
-    );
-    return response.data;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
+  return await makeRequest(
+    "delete",
+    `${API_URL}/recipes/favourites/${recipeId}`
+  );
 };
 
-export const getRecipesFavouritedByUserId = async (userId: string) => {
-  try {
-    const response = await axiosIntance.get(
-      `${API_URL}/recipes/favourites/${userId}`
-    );
-    return response.data;
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
+export const viewRecipe = async (recipeId: string) => {
+  return await makeRequest("post", `${API_URL}/recipes/views/${recipeId}`);
 };
 
 export const deleteRecipe = async (recipeId: string) => {
-  try {
-    const response = await axiosIntance.delete(
-      `${API_URL}/recipes/${recipeId}`
-    );
-    return response.data;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
+  return await makeRequest("delete", `${API_URL}/recipes/${recipeId}`);
 };
